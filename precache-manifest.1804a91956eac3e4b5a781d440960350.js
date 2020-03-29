5eaa3ba85ab68e0ff10729039711077092d92383
Jorge Dias <jorgedsdias@gmail.com>
2020-03-29 12:02:51 -0300

    Updates

---
+++ precache-manifest.1804a91956eac3e4b5a781d440960350.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579df7e0c8f16a30b84df750b83e7c78",
+    "url": "/aircnc-web/index.html"
+  },
+  {
+    "revision": "ba3b38807045a6d89b25",
+    "url": "/aircnc-web/static/css/main.892601fc.chunk.css"
+  },
+  {
+    "revision": "cc716795304ae4860f2c",
+    "url": "/aircnc-web/static/js/2.44f2a03e.chunk.js"
+  },
+  {
+    "revision": "ba3b38807045a6d89b25",
+    "url": "/aircnc-web/static/js/main.8739e061.chunk.js"
+  },
+  {
+    "revision": "445a7ef733085f7343aa",
+    "url": "/aircnc-web/static/js/runtime-main.af48334c.js"
+  },
+  {
+    "revision": "83252e0afaf5f134063345abd6ac8fd7",
+    "url": "/aircnc-web/static/media/background.83252e0a.jpg"
+  },
+  {
+    "revision": "6a6f4b6ebebfabdc283a3a6b417282a0",
+    "url": "/aircnc-web/static/media/camera.6a6f4b6e.svg"
+  },
+  {
+    "revision": "740528028131fdef515dcb930373ccf6",
+    "url": "/aircnc-web/static/media/logo.74052802.svg"
+  }
+]);
