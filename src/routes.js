28440917bda443e5a772587b818b87404b19adc4
Jorge Dias <jorgedsdias@gmail.com>
2020-03-29 13:25:18 -0300

    testando hashrouter

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
@@ -7,12 +7,12 @@ import New from './pages/New';
 
 export default function Routes() {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter basename={process.env.PUBLIC_URL}>
             <Switch>
                 <Route path="/" exact component={Login} />
                 <Route path="/dashboard" component={Dashboard} />
                 <Route path="/new" component={New} />
             </Switch>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
